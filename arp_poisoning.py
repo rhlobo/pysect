127c5032cb4574158278432258e0adb497e94bfc
rhlobo <rhlobo+github@gmail.com>
2014-09-21 01:33:37 -0300

    Assuring functions are run with admin permissions (root, sudo, ...).

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
 
 
+import os
+import sys
 import argh
 
 
+def _assure_root(fn):
+    def wrapper(*args, **kwargs):
+        if not os.geteuid() == 0:
+            sys.exit("\nOnly root can run this script\n")
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@_assure_root
 def poison():
     pass
 
 
+@_assure_root
 def verify():
     pass
 
 
+@_assure_root
 def monitor():
     pass
 
 
+@_assure_root
 def display():
     pass
 
