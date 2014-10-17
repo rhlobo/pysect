54a2c2633505e292db84529401e2736445d4eb5c
rhlobo <rhlobo+github@gmail.com>
2014-10-17 13:49:48 -0300

    Re-added arp poisoning tool's expected operations.

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -39,6 +39,18 @@ def poison(routerIP, victimIP, attackerIP, interface='eth0'):
             time.sleep(2)
 
 
+def verify():
+    pass
+
+
+def monitor():
+    pass
+
+
+def display():
+    pass
+
+
 if __name__ == '__main__':
     logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-    argh.dispatch_commands([poison])
+    argh.dispatch_commands([poison, verify, monitor, display])
